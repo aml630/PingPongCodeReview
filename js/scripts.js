5cd51b66cd33dc4fcf73ac1a92e9d7bbb7913fbe
aml630 <alex.m.larson@gmail.com>
2016-01-22 10:30:15 -0800

    added css min and auto heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
-var listNumbers = function (input) {
-var numbers = []
-for(var i = 1; i<=input; i++) {
-numbers.push(i)
+var listNumbers = function(input) {
+    var numbers = []
+    for (var i = 1; i <= input; i++) {
+        numbers.push(i)
+    }
+    return numbers
 }
-return(numbers)
-}
-var replaceNumbers = function (input) {
-  var replacing = listNumbers(input);
-  var newArray = []
-  var text = ""
-
-  for (var i = 1; i<=replacing.length; i++) {
-        if(i % 15 === 0) {
-          text += "<li>PingPong</li>"
-        }else if(i % 5 === 0) {
-          text += "<li>Pong</li>"
-        }else if(i % 3 === 0) {
-          text += "<li>Ping</li>"
-        }else{
-          text += "<li>" + i + "</li>"
+var replaceNumbers = function(input) {
+    var numArray = listNumbers(input);
+    var text = ""
+    for (var i = 1; i <= numArray.length; i++) {
+        if (i % 15 === 0) {
+            text += "<li>PingPong</li>"
+        } else if (i % 5 === 0) {
+            text += "<li>Pong</li>"
+        } else if (i % 3 === 0) {
+            text += "<li>Ping</li>"
+        } else {
+            text += "<li>" + i + "</li>"
         }
-  }
-  return text
+    }
+    return text
 
 };
 $(document).ready(function() {
-  $("#submit").click(function () {
-    var entry = $("#input").val()
-    var final = replaceNumbers(entry);
-    $("#hidden").show()
-    $("li").text("")
-    $("li").append(final)
-  });
+    $("#submit").click(function() {
+        var entry = $("#input").val()
+        var final = replaceNumbers(entry);
+        $("#hidden").show()
+        $("li").text("")
+        $("li").append(final)
+    });
 
 })
