d58b304e49ec79be356b422f778a866216b5acda
aml630 <alex.m.larson@gmail.com>
2016-01-22 08:15:58 -0800

    listNumber specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+var listNumbers = function (input) {
+var numbers = []
+for(var i = 1; i<=input; i++) {
+numbers.push(i)
+}
+return(numbers)
+}
+
+
+
+
 $(document).ready(function() {
-alert("hellO?")
 
 
 })
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("listNumbers", function () {
+  it("should take a user input and return all numbers leading up to what the user entered into an array.", function () {
+    expect(listNumbers(5)).to.equal[1,2,3,4,5]
+  });
+});
