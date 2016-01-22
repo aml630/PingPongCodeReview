4945e905850ac9fe277f2bfa2868e792b6983a09
aml630 <alex.m.larson@gmail.com>
2016-01-22 08:27:59 -0800

    replaceNumber specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,26 @@ numbers.push(i)
 }
 return(numbers)
 }
-
+var replaceNumbers = function (input) {
+  var replaceNumbers = listNumbers(input);
+  var newArray = []
+  replaceNumbers.forEach(function(number) {
+    if(number % 15 === 0) {
+      number = "PingPong"
+      newArray.push(number)
+    }else if(number % 5 === 0) {
+      number = "Pong"
+      newArray.push(number)
+    }else if(number % 3 === 0) {
+      number = "Ping"
+      newArray.push(number)
+    }else{
+      newArray.push(number)
+    }
+    console.log(newArray)
+    return newArray
+  })
+}
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,9 @@ describe("listNumbers", function () {
     expect(listNumbers(5)).to.equal[1,2,3,4,5]
   });
 });
+
+describe("replaceNumbers", function () {
+  it("should look through the numbers array and replace all values divisible by 3, 5, and 15 with their appropriate strings", function () {
+    expect(replaceNumbers(5)).to.equal[1,2,"Ping",4,"Pong"]
+  });
+});
